f959434498255472ebed31b4c8bd3e47214a1680
jeezlouis <125540102+Jeezlouis@users.noreply.github.com>
2025-08-10 17:18:14 +0100

    implement the home page ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,52 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import Cta from '@/components/Cta'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome To My SaaS App</h1>
-      <Button>
-        Let's get Started
-      </Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+      
+      
+      <section className='home-section'>
+        <CompanionCard 
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard 
+          id="456"
+          name="Countsy the number wizard"
+          topic="Derivatives & Integrals"
+          subject="maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard 
+          id="789"
+          name="Verba the volcabulary builder"
+          topic="language"
+          subject="English Literature"
+          duration={30}
+          color="#bde7ee"
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionList 
+         title="Recently completed sessions"
+         companions={recentSessions}
+         classNames="w-2/3 max-lg:w-full"
+        />
+        <Cta />
+      </section>
+    </main>
   )
 }
 
